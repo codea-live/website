ffabe3d6fe3f5628bb6af7dbc631af9e24514ba3
ADAMJR <adamjf123@gmail.com>
2021-04-01 16:07:28 +0100

    first web server

---
+++ src/web/routes/root-routes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+router.get('/', (req, res) => res.render('index'));
+
--- src/web/server.ts
+++ src/web/server.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import rootRoutes from './routes/root-routes';
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/assets'));
+app.use('/', rootRoutes)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('App running on port ${port}'));
