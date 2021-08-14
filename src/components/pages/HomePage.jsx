62ce04ba25dae732a6feebd40f87ff7e5c762c08
ADAMJR <adamjf123@gmail.com>
2021-08-14 13:26:32 +0100

    Import Tailwind

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,20 +1,10 @@
 const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="bg-bg-primary">
+        a
       </header>
-    </div>
+    </>
   );
 }
 
